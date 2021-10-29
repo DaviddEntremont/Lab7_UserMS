02534d3915583e4fffbe51314ddae565f1967887
David <David@DESKTOP-0SIBBB5.hitronhub.home>
2021-10-29 13:29:49 -0600

    Entered class codes for the classes in the models package. Entered constructors and getters and settings in the role and user classes.

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
-/**
- *
- * @author David
- */
 public class Role {
+    private int rolenumber;
     
+
+public Role() {
+}
+
+public Role(int Rolenumber) {
+    this.rolenumber = Rolenumber;
+}
+
+    public int getRolenumber() {
+        return rolenumber;
+    }
+
+    public void setRolenumber(int rolenumber) {
+        this.rolenumber = rolenumber;
+    }
 }
--- src/java/models/User.java
+++ src/java/models/User.java
@@ -5,10 +5,77 @@
  */
 package models;
 
-/**
- *
- * @author David
- */
-public class User {
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+    
+    private String email;
+    private boolean active;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private Role role;
+    
+    public User() {
+    }
+    
+    public User(String Email, boolean Active, String Firstname, String Lastname, String Password, Role Thisrole) {
+        this.email = Email;
+        this.active = Active;
+        this.firstname = Firstname;
+        this.lastname = Lastname;
+        this.password = Password;
+        this.role = Thisrole;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
     
 }
+
