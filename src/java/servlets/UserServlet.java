5b115c55c9f68d5fa45cdf7e5b3fa4633e03596d
David <David@DESKTOP-0SIBBB5.hitronhub.home>
2021-10-30 14:00:51 -0600

    Began coding out the UserServlets doPost Implementing the functionality to catch the users actions and pass them to the service layer.

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -1,10 +1,14 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import services.UserService;
 
 public class UserServlet extends HttpServlet {
 
@@ -18,6 +22,45 @@ public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        HttpSession session = request.getSession();
+
+        UserService us = new UserService();
+        
+        String action = request.getParameter("action");
+        String email = request.getParameter("email");
+        String active = request.getParameter("active");
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+        Boolean isactive = false;
+        
+        if (active.equals("true")) {
+            isactive = true;
+        }
+        else if (active.equals("false")) {
+            isactive = false;
+        }
+        else {
+            // Improper value
+        }
+        
+        try {
+            switch (action) {
+                case "create":
+                    us.insert(email, isactive, firstname, lastname, password, Integer.parseInt(role));
+                    break;
+                case "update":
+                    us.update(email, isactive, firstname, lastname, password, Integer.parseInt(role));
+                    break;
+                case "delete":
+                    us.delete(email);
+            }
+            request.setAttribute("message", action);
+            } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", "error");
+        }
     }
 
 }
